7e4f01c8ee0f44924030ab6cd9a199a8b977ebd7
kirsty-tortoise <kirstyland29@gmail.com>
2021-02-23 18:44:08 +0000

    fix floodfill browser crashing bug

--- src/tools/floodfill/floodfill.js
+++ src/tools/floodfill/floodfill.js
@@ -5,20 +5,24 @@ export function floodfill(layer, startX, startY, fillColour) {
 
     const startColour = layer.getColourAt(startX, startY);
 
-    const matchStartColor = (x, y) => (startColour.matches(layer.getColourAt(x, y)));
+    if (startColour.matches(fillColour)) {
+        return layer.layer;
+    }
+
+    const matchStartColour = (x, y) => (startColour.matches(layer.getColourAt(x, y)));
 
     while(pixelStack.length) {
         let [x,  y] = pixelStack.pop();
-        while(y > 0 && matchStartColor(x, y - 1)) {
+        while(y > 0 && matchStartColour(x, y - 1)) {
             y--;
         }
         let reachLeft = false;
         let reachRight = false;
-        while(y < canvasHeight-1 && matchStartColor(x, y)) {
+        while(y < canvasHeight-1 && matchStartColour(x, y)) {
             layer.colorPixel(x, y, fillColour);
 
             if(x > 0) {
-                if(matchStartColor(x - 1, y)) {
+                if(matchStartColour(x - 1, y)) {
                     if(!reachLeft) {
                         pixelStack.push([x - 1, y]);
                         reachLeft = true;
@@ -29,7 +33,7 @@ export function floodfill(layer, startX, startY, fillColour) {
             }
         
             if(x < canvasWidth-1) {
-                if(matchStartColor(x + 1, y)) {
+                if(matchStartColour(x + 1, y)) {
                     if(!reachRight) {
                         pixelStack.push([x + 1, y]);
                         reachRight = true;
