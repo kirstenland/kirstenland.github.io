22f0bdb800467bdaead26d68080df7132773090d
Kirsty <kirsten.land@softwire.com>
2021-03-01 16:17:29 +0000

    change drawing + erasing technique

--- src/tools/eraser.js
+++ src/tools/eraser.js
@@ -1,22 +1,39 @@
 export class Eraser {
-    constructor(canvas) {
+    constructor(canvas, _, strokeSize) {
         this.canvas = canvas;
         this.down = false;
+        if (strokeSize) {
+            this.setStrokeSize(strokeSize);
+        }
     }
     start(x, y) {
         this.down = true;
         this.canvas.setColour("rgba(255, 255, 255, 255)");
         this.drawCircle(x, y);
+        this.x = x;
+        this.y = y;
     }
     end() {
         this.down = false;
     }
     move(x, y) {
         if (this.down) {
-            this.drawCircle(x, y);
+            const pointsBetween = 1 + (x + y) / this.radius;
+            const changeX = x - this.x;
+            const changeY = y - this.y;
+            for(let i = 1; i <= pointsBetween; i++) {
+                const inBetweenX = this.x + i * changeX / pointsBetween;
+                const inBetweenY = this.y + i * changeY / pointsBetween;
+                this.drawCircle(inBetweenX, inBetweenY);
+            }
+            this.x = x;
+            this.y = y;
         }
     }
     drawCircle(x, y) {
-        this.canvas.drawCircle([x, y], 2);
+        this.canvas.drawCircle([x, y], this.radius);
+    }
+    setStrokeSize(size) {
+        this.radius = size;
     }
 }
--- src/tools/pen.js
+++ src/tools/pen.js
@@ -11,21 +11,34 @@ export class Pen {
     }
     start(x, y) {
         this.down = true;
-        this.canvas.startLine([x, y]);
+        this.drawCircle(x, y);
+        this.x = x;
+        this.y = y;
     }
     end() {
         this.down = false;
-        this.canvas.endLine();
     }
     move(x, y) {
         if (this.down) {
-            this.canvas.midLine([x, y]);
+            const pointsBetween = 1 + (x + y) / this.radius;
+            const changeX = x - this.x;
+            const changeY = y - this.y;
+            for(let i = 1; i <= pointsBetween; i++) {
+                const inBetweenX = this.x + i * changeX / pointsBetween;
+                const inBetweenY = this.y + i * changeY / pointsBetween;
+                this.drawCircle(inBetweenX, inBetweenY);
+            }
+            this.x = x;
+            this.y = y;
         }
+    }
+    drawCircle(x, y) {
+        this.canvas.drawCircle([x, y], this.radius);
+    }
+    setStrokeSize(size) {
+        this.radius = size;
     }
     setColour(colour) {
         this.canvas.setColour(colour);
-    }
-    setStrokeSize(size) {
-        this.canvas.setStrokeWidth(size);
     }
 }
